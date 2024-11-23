37bf1f200c685c35544f3f5e8041fc50d7520a81
Ahmad0301 <sehroz3040@gmail.com>
2024-11-23 14:32:56 +0500

    make a server file

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express= require('express')
 const mongoose = require('mongoose')
 const app = express()
+const userRouter = require('./Routers/index')
 
 mongoose.connect('mongodb://localhost:27017/')
-.then(()=>console.log('mpngodb is connected'))
+.then(()=>console.log('mongodb is connected'))
+
+app.use(express.json())
+app.use("/user",userRouter)
 
 
 
