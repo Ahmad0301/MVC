b56a023ab359efbe395d96f233d7b9f0e7f776d8
Ahmad0301 <sehroz3040@gmail.com>
2024-11-24 18:49:24 +0500

    function for creating new book

--- Controller/BookController.js
+++ Controller/BookController.js
@@ -1,4 +1,18 @@
 const books = require('../Model/book')
+
+const NewBook =  async (req,resp)=>
+{
+    const data = req.body
+    const object = await books.create(data)
+    resp.json(object)
+}
+
+    const id = req.params.id
+    const object = await books.findByIdAndDelete(id)
+    resp.json(object)
+}
+
+module.exports = 
 {
     NewBook,
     GetAllBooks,
