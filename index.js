d08e7bc3168b5cc72eb05d138f068285fe54c73e
Ahmad0301 <sehroz3040@gmail.com>
2024-11-24 18:52:11 +0500

    import book router from router

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express= require('express')
 const mongoose = require('mongoose')
 const app = express()
 const userRouter = require('./Routers/index')
+const bookrouter = require('./Routers/bookrouter')
 
 mongoose.connect('mongodb://localhost:27017/')
 .then(()=>console.log('mongodb is connected'))
 
 app.use(express.json())
 app.use("/user",userRouter)
+app.use("/book",bookrouter)
 
 
 
