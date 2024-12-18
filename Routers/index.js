4de7421398fdf09fb97ad1f10a695b60a293902b
Ahmad0301 <sehroz3040@gmail.com>
2024-12-18 17:02:42 +0500

    router end points for borrow return an show borrow books

--- Routers/index.js
+++ Routers/index.js
@@ -6,5 +6,8 @@ router.get ('/getAllUser',controller.GetAllUsers)
 router.get ('/userById/:id',controller.GetWithId)
 router.put('/updateuser/:id',controller.UpdateUsers)
 router.delete('/deleteUser/:id',controller.DeletUser)
+router.put('/:Id/borrow/:bookid',controller.borrow)
+router.put('/:id/return/:bookid',controller.returnBook)
+router.get('/:id/BorrowedBooks',controller.borrowedBooks)
 
 module.exports = router
